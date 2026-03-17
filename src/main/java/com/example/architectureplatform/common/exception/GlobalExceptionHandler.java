0b0f8e7295f965627eb1954588b358029048c3c0
Minh Thang Nguyen <nguyenminhthang013@gmail.com>
2026-03-17 03:26:26 -0700

    commit

---
+++ src/main/java/com/example/architectureplatform/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.architectureplatform.common.exception;
+
+import com.example.architectureplatform.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+}
